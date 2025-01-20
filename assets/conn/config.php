b28ea7e330603cea49b865effea19579765f722d
suwardy87 <suwardyser87@gmail.com>
2025-01-21 01:12:00 +0700

    change to template

--- assets/conn/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_ahp";
-$conn = mysqli_connect($host, $user, $pass, $db) or die ("Koneksi Gagal Silahkan Cek Kembali!");
-
-?>
--- index.php
+++ index.php
@@ -1,112 +1,131 @@
 <?php
-session_start();
-include "assets/conn/config.php";
-if (isset($_GET['aksi'])) {
-  if ($_GET['aksi']=='masuk') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $data = mysqli_query($conn,"SELECT * FROM tbl_akun WHERE username = '$username' AND password = '$password'");
-    $row =  mysqli_num_rows($data);
-
-    if ($row > 0) {
-      $a = mysqli_fetch_array($data);
-      if ($a['level']=='Admin') {
-        $_SESSION['username'] = $username;
-        header("location:admin/index.php");
-      }
-    } else {
-      header("location:index.php?login=gagal ");
-    }
-  }
-}
+include_once 'header.php';
+include_once 'includes/nilai.inc.php';
+$pro3 = new Nilai($db);
+$stmt3 = $pro3->readAll();
+include_once 'includes/alternatif.inc.php';
+$pro1 = new Alternatif($db);
+$stmt1 = $pro1->readAll();
+$stmt4 = $pro1->readAll();
+include_once 'includes/kriteria.inc.php';
+$pro2 = new Kriteria($db);
+$stmt2 = $pro2->readAll();
+include_once 'includes/bobot.inc.php';
+$pro5 = new Bobot($db);
+$stmt5 = $pro5->readAll();
 ?>
+		<div class="row">
+		  <div class="col-xs-12 col-sm-12 col-md-2">
+		  	<?php
+			include_once 'sidebar.php';
+			?>
+		  </div>
+		  <div class="col-xs-12 col-sm-12 col-md-10">
+			<div id="container2" style="min-width: 100%; height: 400px; margin: 0 auto"></div>
+		  <br/>
+		<div class="row">
+		  <div class="col-xs-12 col-sm-12 col-md-4">
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Nilai Preferensi</h3>
+			  </div>
+			  <div class="panel-body">
+			    <ol>
+			    	<?php
+					while ($row3 = $stmt3->fetch(PDO::FETCH_ASSOC)){
+					?>
+				  	<li><?php echo $row3['ket_nilai'] ?> (<?php echo $row3['jum_nilai'] ?>)</li>
+				  	<?php
+					}
+				  	?>
+				</ol>
+			  </div>
+			</div>
+		  </div>
+		  <div class="col-xs-12 col-sm-12 col-md-4">
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Kriteria & Bobot</h3>
+			  </div>
+			  <div class="panel-body">
+			    <ol>
+			    	<?php
+					while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
+					?>
+				  	<li><?php echo $row2['nama_kriteria'] ?></li>
+				  	<?php
+					}
+				  	?>
+				</ol>
+			  </div>
+			</div>
+		  </div>
+		  <div class="col-xs-12 col-sm-12 col-md-4">
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Skor Alternatif & Hasil</h3>
+			  </div>
+			  <div class="panel-body">
+			    <ol>
+			    	<?php
+					while ($row1 = $stmt1->fetch(PDO::FETCH_ASSOC)){
+					?>
+				  	<li><?php echo $row1['nama_alternatif'] ?></li>
+				  	<?php
+					}
+				  	?>
+				</ol>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		
+		</div>
+		</div>
+		
+		<footer class="text-center">&copy; 2015</footer>
+	</div>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="assets/css-login/fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="assets/css-login/css/owl.carousel.min.css">
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css-login/css/bootstrap.min.css">
-    
-    <!-- Style -->
-    <link rel="stylesheet" href="assets/css-login/css/style.css">
-
-    <title>SISTEM PENDUKUNG KEPUTUSAN</title>
-  </head>
-  <body ">
-  
-
-  
-  <div class="content" style="background-color: #6295A2";>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 order-md-2">
-          <!-- <img src="assets/im/Domino's Pizza.jpeg" alt="Image" class="img-fluid"> -->
-        </div>
-        <div class="col-md-6 contents">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="mb-4">
-              <h3><strong>Sign In </strong></h3>
-              <p class="mb-4" style="color: black">Sistem Pendukung Keputusan dengan menerapkan metode AHP</p>
-            </div>
-
-            <?php
-            if (isset($_GET['login'])) {
-              if ($_GET['login']=='gagal') {
-                echo "<div class='alert alert-danger' role='alert'><span class='fa fa-times'></span> Username / Password Salah </div>";
-              }
-            }
-            ?>
-
-            <form action="index.php?aksi=masuk" method="post">
-              <div class="form-group first">
-                <label for="username" >Username</label>
-                <input type="text" class="form-control" id="username" name="username" required>
-
-              </div>
-              <div class="form-group last mb-4">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-                
-              </div>
-              
-              <div class="d-flex mb-5 align-items-center">
-                <label class="control control--checkbox mb-0" ><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator" style="background-color: #80B9AD"></div>
-                </label>
-              </div>
-
-              <input type="submit" value="Log In" class="btn text-white btn-block btn-primary" style="background-color: #80B9AD">
-
-            </form>
-            </div>
-          </div>
-          
-        </div>
-        
-      </div>
-    </div>
-  </div>
-
-  
-    <script src="assets/css-login/js/jquery-3.3.1.min.js"></script>
-    <script src="assets/css-login/js/popper.min.js"></script>
-    <script src="assets/css-login/js/bootstrap.min.js"></script>
-    <script src="assets/css-login/js/main.js"></script>
-  </body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/highcharts.js"></script>
+	<script src="js/exporting.js"></script>
+	<script>
+	var chart1; // globally available
+	$(document).ready(function() {
+	      chart1 = new Highcharts.Chart({
+	         chart: {
+	            renderTo: 'container2',
+	            type: 'column'
+	         },  
+	         title: {
+	            text: 'Grafik Perangkingan '
+	         },
+	         xAxis: {
+	            categories: ['Alternatif']
+	         },
+	         yAxis: {
+	            title: {
+	               text: 'Jumlah Nilai'
+	            }
+	         },
+	              series:            
+	            [
+	            <?php
+	            while ($row4 = $stmt4->fetch(PDO::FETCH_ASSOC)){
+	                  ?>
+	                 //data yang diambil dari database dimasukan ke variable nama dan data
+	                 //
+	                  {
+	                      name: '<?php echo $row4['nama_alternatif'] ?>',
+	                      data: [<?php echo $row4['hasil_akhir'] ?>]
+	                  },
+	                  <?php } ?>
+	            ]
+	      });
+	   });  
+	   </script>
+	</body>
 </html>
